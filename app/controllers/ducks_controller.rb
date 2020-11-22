923f377854f5f72abe2cfa3cdc0058c6bed49d17
JasonAkiraJordan <jasonajordan0@gmail.com>
2020-11-22 17:03:45 -0500

    lab

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -1,7 +1,45 @@
 class DucksController < ApplicationController
 
-  def index
+  def index #show list of all 
     @ducks = Duck.all
   end
+
+  def show #show single and ducks owned
+    @duck = Duck.find(params[:id])
+  end 
+
+  def new 
+    @duck = Duck.new
+    @students = Student.all
+  end 
+
+  def create #log new duck needs validations 
+    @duck = Duck.new(duck_params) 
+
+    if @duck.save 
+      redirect_to duck_path(@duck)
+    else 
+      render :new 
+    end 
+
+  end 
+
+  def edit # duck can edit 
+    @duck = Duck.find(params[:id])
+    @students = Student.all
+  end 
+
+  def update 
+    @duck = Duck.find(params[:id])
+    @duck.update(duck_params)
+    redirect_to duck_path(@duck_params)
+
+  end 
+
+  private 
+
+  def duck_params 
+    params.require(:duck).permit(:name, :description, :student_id)
+  end 
 
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,45 @@
 class StudentsController < ApplicationController
 
-  def index
+  def index #show list of all 
     @students = Student.all
   end
 
+  def show #show single and ducks owned
+    @student = Student.find(params[:id])
+    @ducks = Duck.all
+  end 
+
+  def new 
+    @student = Student.new
+  end 
+
+  def create #log new student needs validations 
+    @student = Student.new(student_params) 
+
+    if @student.save 
+      redirect_to student_path(@student)
+    else 
+      render :new 
+    end 
+
+  end 
+
+  def edit # student can edit 
+    @student = Student.find(params[:id])
+  end 
+
+  def update 
+    @student = student.find(params[:id])
+    @student.update(student_params)
+    redirect_to student_path(@student_params)
+
+  end 
+
+
+  private 
+
+  def student_params 
+    params.require(:student).permit(:name, :mod)
+  end 
 end
+
